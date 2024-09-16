f5c318bd85985deef1afb881c6ca4ddc10b4b6a6
Arjun G <arjungopinath06@gmail.com>
2024-09-16 18:34:58 +0530

    fix. css changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,32 @@
 import React from "react";
-import food1 from "../images/food1.avif"
-import { Link } from "react-router-dom";
-
+import food1 from "../images/food1.avif";
+import { useNavigate } from "react-router-dom";
+import "./css/Header.css";
 
 const Header = () => {
-    return(
-        <header>
-            <section>
-            <div>
-            <h2>Little Lemon</h2>
-            <h3>Chicago</h3>
-            <p>We are a family owned Mediterraneran restaurant, focused on traditional recipes servred with a modern twist.</p>
-            <Link to="/Booking"><button aria-label="On Click">Reserve Table</button></Link>
-            </div>
-            <div>
-                <img src={food1} />
-            </div>
-            </section>
-        </header>
-    )
-}
+  const navigate = useNavigate();
+  return (
+    <header>
+      <section>
+        <div className="content">
+          <h2>Little Lemon</h2>
+          <h3>Chicago</h3>
+          <p>
+            We are a family owned Mediterraneran restaurant, focused on
+            traditional recipes servred with a modern twist.
+          </p>
+          <button
+            className="reserve"
+            aria-label="Reserve Table"
+            onClick={() => navigate("/Booking")}
+          >
+            Reserve Table
+          </button>
+        </div>
+        <img src={food1} alt="Food" />
+      </section>
+    </header>
+  );
+};
 
 export default Header;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,48 @@
 import React from "react";
-import littlelemon_logo from "../images/littlelemon_logo.png"
+import littlelemon_logo from "../images/littlelemon_logo.png";
+import { Link } from "react-router-dom";
+import "./css/Nav.css";
 
 const Nav = () => {
-    return(
-        <nav>
-            <img src={littlelemon_logo} alt="Little Lemon Logo" ></img>
-            <ul>
-                <li><a>Home</a></li>
-                <li><a>About</a></li>
-                <li><a>Menu</a></li>
-                <li><a>Reservations</a></li>
-                <li><a>Order Online</a></li>
-                <li><a>Login</a></li>
-
-            </ul>
-        </nav>
-    )
-}
+  return (
+    <nav>
+      <Link to="/">
+        <img src={littlelemon_logo} alt="Little Lemon Logo"></img>
+      </Link>
+      <ul>
+        <li>
+          <Link className="links" to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="links" to="/about">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link className="links" to="/menu">
+            Menu
+          </Link>
+        </li>
+        <li>
+          <Link className="links" to="/reserve">
+            Reservations
+          </Link>
+        </li>
+        <li>
+          <Link className="links" to="/order">
+            Order Online
+          </Link>
+        </li>
+        <li>
+          <Link className="links" to="/login">
+            Login
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Nav;
